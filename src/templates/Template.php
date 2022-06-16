32959c7b616bedaef3caa53db24554684758aa62
Sir7lease github <hammoumi.abdalaziz@gmail.com>
2022-06-16 10:38:37 +0200

    [Dev] Template Save Feature added.

--- src/templates/Template.php
+++ src/templates/Template.php
@@ -3,44 +3,69 @@ namespace Aham\EzForm\Templates;
 
 use Aham\EzForm\Form;
 use Aham\EzForm\FormBuilder;
+use function PHPUnit\Framework\isInstanceOf;
 
 class Template extends FormBuilder
 {
-    private const MSG_NO_TEMPLATE = 'There is no template saved yet.';
     private const TEMPLATES_JSON_FILE_PATH = './src/templates/template.json';
     private $contentFile = [];
+    private string|Form $template;
 
     public function __construct()
     {
        $this->getContent();
     }
 
-    public function getListTemplates(): string
+    public function getListTemplates(): self
     {
-         if( !$this->contentFile )
-             return self::MSG_NO_TEMPLATE;
+        if ( !isset( $this->contentFile ) ) {
+            echo "There is no template saved yet. Use saveTemplate() method to save a template." . PHP_EOL;
+            exit;
+        }
 
-         return implode( '<br/>', array_keys( $this->contentFile ) ) . '<br/><br/>';
+        echo implode( '<br/>', array_keys( $this->contentFile ) ) . '<br/><br/>';
+        return $this;
     }
 
-    public function getTemplate(string $nameTemplate): string|Form
+    public function getTemplate(string $nameTemplate): ?Form
     {
-        if( !$this->contentFile )
-            return self::MSG_NO_TEMPLATE;
+        if( !isset($this->contentFile) ) {
+            echo "There is no template saved yet. Use saveTemplate() method to save a template.";
+            exit;
+        }
+
+        if( !array_key_exists( $nameTemplate, $this->contentFile ) ) {
+            echo "The template '$nameTemplate' doesn't exist. Use getListTemplates() method to see all the templates saved.";
+            exit;
+        }
 
         $this->getContent();
-        return unserialize( $this->contentFile[ $nameTemplate ] );
+        $this->template = @unserialize($this->contentFile[$nameTemplate]);
+        if ( $this->template ) {
+            return $this->template;
+        } else {
+            echo "The template '$nameTemplate' is corrupted and therefore can't be used. You can delete the template with removeTemplate() method.";
+            exit;
+        }
     }
 
-    public function renderTemplate(string $nameTemplate): ?string
+    /*public function renderTemplate(): ?string
     {
-        if( !$this->contentFile )
-            return self::MSG_NO_TEMPLATE;
-        echo $this->buildForm($this->getTemplate($nameTemplate))->renderForm();
-    }
+        if( !isset($this->template) ) {
+            echo "You're trying to render a template before getting it. Use getTemplate() method before rendering";
+            exit();
+        }
+        echo $this->buildForm($this->template)->renderForm();
+    }*/
 
-    public function saveTemplate(string $nameTemplate, Form $form)
+    public function saveTemplate(string $nameTemplate, Form $form): self
     {
+        $fileinfo = 'no_file_info';
+        $backtrace = debug_backtrace();
+        if ( !empty( $backtrace[0] ) && is_array( $backtrace[0] ) ) {
+            $fileinfo = $backtrace[0]['file'] . ":" . $backtrace[0]['line'];
+        }
+
         if( !$this->contentFile ) {
             file_put_contents(self::TEMPLATES_JSON_FILE_PATH, '{}');
             $this->getContent();
@@ -48,15 +73,28 @@ class Template extends FormBuilder
 
         if( !array_key_exists( $nameTemplate, $this->contentFile ) ) {
             $this->contentFile[ $nameTemplate ] = serialize( $form );
-            $this->putContent();
+
+            if( $this->putContent() ) {
+                echo <<<TEMPLATE_SAVED
+                        Template '$nameTemplate' is saved now.
+                        You can remove saveTemplate() method in $fileinfo".
+                     TEMPLATE_SAVED;
+            }
+        } else {
+            echo <<<TEMPLATE_SAVED
+                        Template '$nameTemplate' already exist.
+                        Remove saveTemplate() method in $fileinfo".
+                     TEMPLATE_SAVED;
         }
+        return $this;
     }
 
-    public function removeTemplate(string $nameTemplate)
+    public function removeTemplate(string $nameTemplate): self
     {
         $this->getContent();
         unset($this->contentFile[$nameTemplate]);
         $this->putContent();
+        return $this;
     }
 
     private function getContent(): void
@@ -64,9 +102,9 @@ class Template extends FormBuilder
         $this->contentFile = json_decode( file_get_contents(self::TEMPLATES_JSON_FILE_PATH), true );
     }
 
-    private function putContent(): void
+    private function putContent(): int|bool
     {
-        file_put_contents( self::TEMPLATES_JSON_FILE_PATH, json_encode( $this->contentFile ) );
+        return ( file_put_contents( self::TEMPLATES_JSON_FILE_PATH, json_encode( $this->contentFile ) ) )??false;
     }
 
 }
